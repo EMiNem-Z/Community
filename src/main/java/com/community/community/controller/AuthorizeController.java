2b4c736cd8568290e11a6ac2ab9f23cfa0d0dc57
zr <971418846@qq.com>
2020-01-04 13:43:24 +0800

    login

---
+++ src/main/java/com/community/community/controller/AuthorizeController.java
@@ -0,0 +1,30 @@
+package com.community.community.controller;
+
+import com.community.community.dto.AccessTokenDTO;
+import com.community.community.dto.GithubUser;
+import com.community.community.provider.GithubProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class AuthorizeController {
+
+    @Autowired
+    private GithubProvider githubProvider;
+
+    @GetMapping("/callback")
+    public String callback(@RequestParam(name="code") String code,@RequestParam(name="state") String state){
+        AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
+        accessTokenDTO.setClient_id("9cd9d3d946beac6a79c9");
+        accessTokenDTO.setClient_secret("fa190b714dcb031a907dd80f9191a12c0cee66dc");
+        accessTokenDTO.setCode(code);
+        accessTokenDTO.setRedirect_uri("http://localhost:8080/callback");
+        accessTokenDTO.setState(state);
+        String accessToken = githubProvider.getAccessToken(accessTokenDTO);
+        GithubUser user = githubProvider.getUser(accessToken);
+        System.out.println(user.getLogin());
+        return "index";
+    }
+}
---
+++ src/main/java/com/community/community/dto/GithubUser.java
@@ -0,0 +1,22 @@
+package com.community.community.dto;
+
+public class GithubUser {
+    private String login;
+    private Long id;
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
